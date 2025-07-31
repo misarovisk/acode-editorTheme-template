c6bba9486b0326c4d7402bbe3a33b76337771cd1
misarovisk <misaelcostasoares10@gmail.com>
2025-07-30 23:48:30 -0300

    Atualizar o main.js

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ const { editor } = editorManager
  * monokai
  */
 // only modify this name and nothing else 
-const themeName = "vscode-dark"
+const themeName = "moon-dark"
 // go to style.scss and change colors there
 
 ace.define(`ace/theme/${themeName}.css`, ["require", "exports", "module"], function (require, exports, module) { module.exports = style }),
